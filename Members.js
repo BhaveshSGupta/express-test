5fca980fc709e5f6d79d36322a674f2140dd32f7
Bhavesh S. Gupta <BhaveshSGUpta@gmail.com>
2019-09-23 22:23:28 +0530

    All member using external file

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [{
+    id:1,
+    name:'test1',
+    email:'test1@',
+    status:'active'
+},{
+    id:2,
+    name:'test2',
+    email:'test2@',
+    status:'active'
+},{
+    id:3,
+    name:'test3',
+    email:'test3@',
+    status:'active'
+},{
+    id:4,
+    name:'test4',
+    email:'test4@',
+    status:'active'
+},{
+    id:5,
+    name:'test5',
+    email:'test5@',
+    status:'active'
+}]
+
+module.exports = members
