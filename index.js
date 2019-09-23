d2db4287622327cfe6d6d30ab7019d8ed9cf84c5
Bhavesh S. Gupta <BhaveshSGUpta@gmail.com>
2019-09-23 22:07:09 +0530

    sending a file

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 
 app.get('/', (req,res)=>{
-    res.send('<h1>Hello World!</h1>')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+
 })
 
 const PORT = process.env.PORT || 3000
