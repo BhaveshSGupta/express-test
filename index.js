f18889694952cc84c128954e60768b05498da089
Bhavesh S. Gupta <BhaveshSGUpta@gmail.com>
2019-09-23 22:20:07 +0530

    api/members

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const path = require('path')
 
+const members = [{
+    id:1,
+    name:'test1',
+    email:'test1@',
+    status:'active'
+},{
+    id:2,
+    name:'test2',
+    email:'test2@',
+    status:'active'
+},{
+    id:3,
+    name:'test3',
+    email:'test3@',
+    status:'active'
+},{
+    id:4,
+    name:'test4',
+    email:'test4@',
+    status:'active'
+},{
+    id:5,
+    name:'test5',
+    email:'test5@',
+    status:'active'
+}]
+
 const app = express()
 
-// app.get('/', (req,res)=>{
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+// This route gets all members
+app.get('/api/members', (req, res) =>  res.json(members))
+
+
+
 
 //using static folder
 app.use(express.static(path.join(__dirname, 'public')))
