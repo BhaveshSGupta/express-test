261a1d5e0895b554d9ee999a3b885a9282ae90c1
Bhavesh S. Gupta <BhaveshSGUpta@gmail.com>
2019-09-23 22:51:30 +0530

    Member api Route Moving all route to different folder and managing it from there

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
 const express = require('express')
 const path = require('path')
-const members = require('./Members')
+
 const logger = require('./middleware/logger')
 
 const app = express()
 
-
-
 //init MiddleWare
 //app.use(logger)
 
-// This route to get individual member
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id))
-    if(found){
-        res.json(members.filter(member  => member.id === parseInt(req.params.id)))
-    }
-    else{
-     res.status(400).json({msg:`No Member with id of ${req.params.id} found`})   
-    }
-
-})
-
-// This route gets all members
-app.get('/api/members', (req, res) =>  res.json(members))
-
 //using static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Memeber api route
+app.use('/api/members', require('./routes/api/members'))
 
 const PORT = process.env.PORT || 3000
 
---
+++ routes/api/members.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const members = require('../../Members')
+// This route to get individual member
+router.get('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    if(found){
+        res.json(members.filter(member  => member.id === parseInt(req.params.id)))
+    }
+    else{
+     res.status(400).json({msg:`No Member with id of ${req.params.id} found`})   
+    }
+
+})
+
+// This route gets all members
+router.get('/', (req, res) =>  res.json(members))
+
+module.exports = router 
