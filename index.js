8ed2c7251e0aec8f1f53e9de09c78fae0b2531a1
Bhavesh S. Gupta <BhaveshSGUpta@gmail.com>
2019-09-23 22:11:47 +0530

    static folder

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const path = require('path')
 
 const app = express()
 
+// app.get('/', (req,res)=>{
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// })
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-
-})
+//using static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 3000
-
 
 app.listen(PORT, () => console.log(`server is on ${PORT}`))
