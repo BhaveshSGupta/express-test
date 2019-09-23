c0f2741958a3cd320d6cd3d9c3a0f6660e4c3135
Bhavesh S. Gupta <BhaveshSGUpta@gmail.com>
2019-09-23 22:02:12 +0530

    add nodemon and simple get request

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+
 const app = express()
-const port = 3000
 
-app.get('/', function (req,res){
-    res.send("Hello World")
+
+app.get('/', (req,res)=>{
+    res.send('<h1>Hello World!</h1>')
 })
 
-app.listen(port,function(){
-    console.log("Its on")
-})
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, () => console.log(`server is on ${PORT}`))
