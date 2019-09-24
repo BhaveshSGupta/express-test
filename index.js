9b3c804981f5e6630272f2fa04008b0b8dbd11e9
Bhavesh S. Gupta <BhaveshSGUpta@gmail.com>
2019-09-24 11:26:12 +0530

    add handle barview engine

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-
+const exphbs =  require('express-handlebars')
 const logger = require('./middleware/logger')
 
 const app = express()
@@ -12,6 +12,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// Handlebar Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 //using static folder
 app.use(express.static(path.join(__dirname, 'public')))
